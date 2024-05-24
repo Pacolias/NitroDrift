963fb13df8c4819765365e255e4967e5852c6af2
AngelManuelSG <amsg150103@gmail.com>
2024-05-25 00:25:13 +0200

    Mejora del gradle e implementación pruebas de la clase circuito

--- NiroDrift/core/src/test/com/nitrodrift/CircuitoTest.java
+++ NiroDrift/core/src/test/com/nitrodrift/CircuitoTest.java
@@ -1,21 +1,36 @@
-/* package com.nitrodrift;
+/*package com.nitrodrift;
 
 import com.badlogic.gdx.graphics.Texture;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CircuitoTest {
+    Circuito c;
+    Texture t;
 
+    @BeforeEach
+    public void setUp(){
+        t = new Texture("circuito1.jpg");
+        c = new Circuito(1, "El Jarama", 0, t);
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        t = null;
+        c = null;
+    }
     @Test
     void setLongitudTest() {
-        Texture t = new Texture("circuito1.jpg");
-        Circuito c = new Circuito(1, "El Jarama", 0, t);
         c.setLongitud(1239);
         assertEquals(1239, c.getLongitud());
     }
 
     @Test
-    void getImagenTest() {
+    void getImagen() {
+        assertEquals(c.getImagen(), t);
     }
 } */
---
+++ NiroDrift/core/src/test/com/nitrodrift/VehiculoTest.java
@@ -0,0 +1,10 @@
+/*
+package com.nitrodrift;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehiculoTest {
+
+
+}*/
